1bcb7e575c67dcdb91341cd57d58e27892add7b9
이아현 <suk03029@gmail.com>
2021-11-04 12:27:39 +0900

    adapter Kotlin파일 추가 ArrayAdapter상속

---
+++ app/src/main/java/com/nepplus/jicbangcopy20211104/RoomAdapter/RoomAdapter.kt
@@ -0,0 +1,7 @@
+package com.nepplus.jicbangcopy20211104.RoomAdapter
+
+import android.widget.ArrayAdapter
+import com.nepplus.jicbangcopy20211104.Roomdata.Roomdata
+
+class RoomAdapter : ArrayAdapter<Roomdata>(){
+}
--- app/src/main/java/com/nepplus/jicbangcopy20211104/Roomdata/Roomdata.kt
+++ app/src/main/java/com/nepplus/jicbangcopy20211104/Roomdata/Roomdata.kt
@@ -1,7 +1,6 @@
 package com.nepplus.jicbangcopy20211104.Roomdata
 
-class Roomdata (val price:Int, val address:String, floor:Int, val description:String)
+class Roomdata (val price:Int, val address:String, floor:Int, val description:String){
 
 
-{
 }
